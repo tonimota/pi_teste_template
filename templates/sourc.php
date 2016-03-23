7c1cfe9052f7cb9087fc1fb302c12426e8bcc8c2
unknown <tonielton.rmota@CAS411475W1032.senacsp.edu.br>
2016-03-23 20:24:17 -0300

    mudanca nos arquivos

--- templates/sourc.php
+++ templates/sourc.php
@@ -1,18 +1,23 @@
 <?php
-	$logins = array('Toni' => '1234',
-					'Julia'=> '546',
-					'Marcelo' => '987');
+$logins = array('Toni' => '1234',
+				'Julia'=> '546',
+				'Marcelo' => '987');
 
-	if (isset($_POST['btn_send'])) {
-		if (isset($logins[$_POST['name']])) {
-			if ($logins[$_POST['name']] == $_POST['password']) {
-				include('templates/bienvienido.html');			
-				}
-		}		
-	}
-	else {
-		
+if (isset($_POST['btn_send'])) {
+	if (isset($logins[$_POST['name']])) {
+		if ($logins[$_POST['name']] == $_POST['password']) {
+			include('bienvienido.php');			
+		} else {
+			$msg = 'Usúario ou senha inválido';
+			include('index.php');
+		}
+	} else {
 		$msg = 'Usúario ou senha inválido';
-		include('templates/index.html');
+		include('index.php');
 	}
+}
+else {
+	include('index.php');
+}
+
 ?>
